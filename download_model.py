aaefd64a421865d87ede98ac8647c084d136c1d6
Vani Gupta <vanig0344@gmail.com>
2026-06-16 14:38:17 +0530

    Switch model hosting from Google Drive to HF Hub

--- download_model.py
+++ download_model.py
@@ -1,15 +1,13 @@
-import gdown
 import os
+from huggingface_hub import hf_hub_download
+import shutil
 
 MODEL_PATH = "model/lungvision_model.pth"
-
 if not os.path.exists(MODEL_PATH):
     print("Downloading model...")
-    gdown.download(
-        id="1c5V9OKM1ONNAXWyWIwrO36qW6sMLhHbK",  # ← paste your ID here
-        output=MODEL_PATH,
-        quiet=False
-    )
+    path = hf_hub_download(repo_id="vanig/lungvision-model", filename="lungvision_model.pth")
+    os.makedirs("model", exist_ok=True)
+    shutil.copy(path, MODEL_PATH)
     print("Model downloaded successfully!")
 else:
     print("Model already exists, skipping download.")
